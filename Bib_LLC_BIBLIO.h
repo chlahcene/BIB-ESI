15f31caf062b15a58b82e079155fb11c1606f697
ch9lahcene <gl_chaklala@esi.dz>
2018-09-14 11:34:36 +0100

    version 1.0.0

---
+++ Bib_LLC_BIBLIO.h
@@ -0,0 +1,335 @@
+#ifndef _Bib_LLC_BIBLIO_H_
+#define	_Bib_LLC_BIBLIO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Bib_Standare.h"
+
+typedef struct date date ;
+typedef struct maillon_Livre maillon_Livre ;
+typedef struct maillon_Emprunteur maillon_Emprunteur ;
+typedef struct maillon_Emprunt maillon_Emprunt ;
+
+struct date{
+    int days;
+    int month;
+    int years;
+};
+
+struct maillon_Emprunteur{
+    char nom[50];
+    char prenom[50];
+    char adresseActuelle[50];
+    char nDeTelephone[30];
+    int ordreInscription;
+    date dateDeInscription;
+    int penaliterTotal;
+    int penaliter;
+    int Emprunter;
+    int NbreDeEmprunt;
+    maillon_Emprunt* Emprunt;
+    maillon_Emprunteur *suiv;
+};
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+struct maillon_Livre{
+    int cote;
+    char titre[50];
+    char auteur[50];
+    date dateEdition;
+    char editeur[50];
+    char motCle[70];
+    char resume[700];
+    int nbrDeEmprunt;
+    int disponible;
+    maillon_Emprunt* Emprunt;
+    maillon_Livre *suiv;
+};
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+struct maillon_Emprunt{
+    maillon_Emprunteur *Emprunteur;
+    maillon_Livre *livreEmprunte;
+    date dateDeEmprunt;
+    date dateDeRetour;
+    int penaliter;
+    int livreRendu;
+    maillon_Emprunt *suiv;
+};
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**---------------------------------------------- MACHINE ABSTREATE DE FICHE LIVRE -------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void allouer_Livre (maillon_Livre**nouveau)
+      {
+         *nouveau = (maillon_Livre*)malloc(sizeof(maillon_Livre)) ;
+      }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void liberer_Livre(maillon_Livre**ancien)
+  {
+    free(*ancien);
+    *ancien=NULL;
+  }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void aff_adr_Livre(maillon_Livre*p,maillon_Livre*q)
+     {
+       p->suiv=q;
+     }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+maillon_Livre * suivant_Livre(maillon_Livre*p)
+    {
+     return (p->suiv);
+    }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+int longeur_Livre ( maillon_Livre *p)/// "LONGEUR" EST UNE FONCTION PERMET DE S'AVOIR LA LONGEUR D'UNE LISTE POINTE PAR *P
+{
+       int l=0;
+       while(p!=NULL)
+           {
+              p=suivant_Livre(p);
+              l++;
+           }
+      return l;
+     }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void AccPos2_Livre (int pos , maillon_Livre**p , maillon_Livre**q , maillon_Livre* liste) /// DONNE L'ADRESSE DU PTR DE MAILLON DE POSITION POS ET L'ADRESSE DU PTR DU MAILLON QUI LE PRECEDE
+{
+    int i=1 ;
+    *p= liste , *q = NULL ;
+    while ( *p != NULL && i != pos)
+    {
+        *q = *p ;
+        *p = suivant_Livre(*p) ;
+        i++ ;
+    }
+}
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+maillon_Livre* AccPos1_Livre (int pos , maillon_Livre* liste ) // DONNE L'ADRESSE DU PTR DE MAILLON DE POSITION pos
+{
+    int i = 1 ;
+    maillon_Livre* p = liste ;
+
+    while ( i != pos && p != NULL)
+    {
+        p = suivant_Livre(p);
+        i++;
+    }
+    return p ;
+}
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void viderListe_Livre(maillon_Livre**liste) /// Libere tout les maillon d'une liste
+{
+    maillon_Livre *p ;
+    while ( *liste != NULL)
+    {
+        p = *liste ;
+        *liste = suivant_Livre(*liste);
+        liberer_Livre(&p);
+    }
+}
+
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**---------------------------------------------- MACHINE ABTREATE DE FICHE EMPRUNTUEUR -------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void allouer_Emprunteur (maillon_Emprunteur**nouveau)
+      {
+         *nouveau = (maillon_Emprunteur*)malloc(sizeof(maillon_Emprunteur)) ;
+      }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void liberer_Emprunteur(maillon_Emprunteur**ancien)
+  {
+    free(*ancien);*ancien=NULL;
+  }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void aff_adr_Emprunteur(maillon_Emprunteur*p,maillon_Emprunteur*q)
+     {
+       p->suiv=q;
+     }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+maillon_Emprunteur * suivant_Emprunteur(maillon_Emprunteur*p)
+    {
+     return (p->suiv);
+    }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+int longeur_Emprunteur ( maillon_Emprunteur *p)/// "LONGEUR" EST UNE FONCTION PERMET DE S'AVOIR LA LONGEUR D'UNE LISTE POINTE PAR *P
+{
+       int l=0;
+       while(p!=NULL)
+           {
+              p=suivant_Emprunteur(p);
+              l++;
+           }
+      return l;
+     }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void AccPos2_Emprunteur (int pos , maillon_Emprunteur**p , maillon_Emprunteur**q , maillon_Emprunteur* liste) // DONNE L'ADRESSE DU PTR DE MAILLON DE POSITION POS ET L'ADRESSE DU PTR DU MAILLON QUI LE PRECEDE
+{
+    int i=1 ;
+    *p= liste , *q = NULL ;
+    while ( *p != NULL && i != pos)
+    {
+        *q = *p ;
+        *p = suivant_Emprunteur(*p) ;
+        i++ ;
+    }
+}
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+maillon_Emprunteur* AccPos1_Emprunteur (int pos , maillon_Emprunteur* liste ) // DONNE L'ADRESSE DU PTR DE MAILLON DE POSITION pos
+{
+    int i = 1 ;
+    maillon_Emprunteur* p = liste ;
+
+    while ( i != pos && p != NULL)
+    {
+        p = suivant_Emprunteur(p);
+        i++;
+    }
+    return p ;
+}
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void viderListe_Emprunteur(maillon_Emprunteur**liste) // Libere tout les maillon d'une liste
+{
+    maillon_Emprunteur *p ;
+    while ( *liste != NULL)
+    {
+        p = *liste ;
+        *liste = suivant_Emprunteur(*liste);
+        liberer_Emprunteur(&p);
+    }
+}
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**---------------------------------------------- MACHINE ABTREATE DE FICHE EMPRUNT -----------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+
+void allouer_Emprunt (maillon_Emprunt**nouveau)
+      {
+         *nouveau = (maillon_Emprunt*)malloc(sizeof(maillon_Emprunt)) ;
+      }
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void liberer_Emprunt(maillon_Emprunt**ancien)
+  {
+    free(*ancien);*ancien=NULL;
+  }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void aff_adr_Emprunt(maillon_Emprunt*p,maillon_Emprunt*q)
+     {
+       p->suiv=q;
+     }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+maillon_Emprunt * suivant_Emprunt(maillon_Emprunt*p)
+    {
+     return (p->suiv);
+    }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+int longeur_Emprunt ( maillon_Emprunt *p)/// "LONGEUR" EST UNE FONCTION PERMET DE S'AVOIR LA LONGEUR D'UNE LISTE POINTE PAR *P
+{
+       int l=0;
+       while(p!=NULL)
+           {
+              p=suivant_Emprunt(p);
+              l++;
+           }
+      return l;
+     }
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void AccPos2_Emprunt (int pos , maillon_Emprunt**p , maillon_Emprunt**q , maillon_Emprunt* liste) // DONNE L'ADRESSE DU PTR DE MAILLON DE POSITION POS ET L'ADRESSE DU PTR DU MAILLON QUI LE PRECEDE
+{
+    int i=1 ;
+    *p= liste , *q = NULL ;
+    while ( *p != NULL && i != pos)
+    {
+        *q = *p ;
+        *p = suivant_Emprunt(*p) ;
+        i++ ;
+    }
+}
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+maillon_Emprunt* AccPos1_Emprunt (int pos , maillon_Emprunt* liste ) /// DONNE L'ADRESSE DU PTR DE MAILLON DE POSITION pos
+{
+    int i = 1 ;
+    maillon_Emprunt* p = liste ;
+
+    while ( i != pos && p != NULL)
+    {
+        p = suivant_Emprunt(p);
+        i++;
+    }
+    return p ;
+}
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+void viderListe_Emprunt(maillon_Emprunt**liste) /// Libere tout les maillon d'une liste
+{
+    maillon_Emprunt *p ;
+    while ( *liste != NULL)
+    {
+        p = *liste ;
+        *liste = suivant_Emprunt(*liste);
+        liberer_Emprunt(&p);
+    }
+}
+
+
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+/**--------------------------------------------------------------------------------------------------------------------------------**/
+
+
+
+
+
+#endif
---
+++ Bib_Standare.h
@@ -0,0 +1,201 @@
+#ifndef _Bib_Standare_H_
+#define	_Bib_Standare_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int Max(int a,int b){
+    if (a<b) return b;
+        else return a;
+}
+int Min(int a,int b){
+    if (a>b) return b;
+        else return a;
+}
+char* UpperCase(char t[]){
+
+    /// TRANFERT LES LETTRES EN MINISCULE
+
+    char p[2000];
+    int i=0;
+    while(t[i]!=0){
+        p[i]=toupper(t[i]);
+        i++;
+    }
+    p[i]=0;
+    return p;
+}
+void InitializerTableauxInt(int t[][50],int n,int n2,int m){
+    /// Initializer un tableaux de n*n2 cases par valeurs m
+    int i,j;
+    for (i=0;i<n;i++)
+        for (j=0;j<n2;j++)
+        t[i][j]=m;
+
+}
+int MaxJour(int y,int m){
+
+    /** DONNEZ LE MAXIMAN JOUR DE LA MOIS
+     ** IL PRENDS COMPS LE ANNES DES 29 ET 28 FIVIRIER
+     **/
+
+     switch (m) {
+
+     case 1: return 31;
+     case 2: return ((y%4)==0) ? 29 : 28 ;
+     case 3: return 31;
+     case 4: return 30;
+     case 5: return 31;
+     case 6: return 30;
+     case 7: return 31;
+     case 8: return 31;
+     case 9: return 30;
+     case 10: return 31;
+     case 11: return 30;
+     case 12: return 31;
+
+     }
+}
+char* LowerCase(char t[]){
+
+    /// TRANFERT LES LETTRES EN MINISCULE
+
+    char p[2000];
+    int i=0;
+    while(t[i]!=0){
+        p[i]=tolower(t[i]);
+        i++;
+    }
+    p[i]=0;
+    return p;
+}
+void SupprimerChar(char m[],int Taille ,int pos ){
+
+    /** SUPPERMER UN CARACTERE A LA POSTION POS
+     ** FAIRE DECALER LE RESTE DE CARACTERE APRES POSTION POS
+     **/
+
+        int i;
+        for (i=pos;i<(Taille-1);i++) m[i]=m[i+1];
+        m[Taille-1]=0;
+
+}
+int NbreMot(char ch[]){
+
+    /// COMPTER LE NOMBRE DES MOTS DANS CHAIN CH
+
+    int cpt=0,i=0,rep=0;
+    while(ch[i]!=0){
+        if (ch[i]==' '){
+            if (!rep) cpt++;
+            rep=1;
+            }else rep=0;
+        i++;
+    }
+    if (!rep) cpt++;
+    return cpt;
+}
+void AjoutezChar(char m[],int Taille ,int pos ,char c){
+
+    /** AJOUTEZ UN CARACTERE C A LA POSTION POS
+     ** FAIRE DECALER A GAUCHE LE RESTE DE CARACTERE APRES POSTION POS
+     **/
+
+    int i;
+    for (i=Taille-1;i>=pos;i--) m[i+1]=m[i] ;
+    m[pos]=c;
+    m[Taille+1]=0;
+
+}
+void DecopPhrase(char ch[],char* m[],int *NbrMot){
+
+    /// DECOPOSER PHRASE EN MOTS
+
+    (*NbrMot)=0;
+    int i=0,j=0,rep=1;
+     m[(*NbrMot)]=malloc(20*sizeof(char));
+    while(ch[i]!=0){
+        if (ch[i]==' '){
+            if (!rep) {
+                m[*NbrMot][j]=0;
+                (*NbrMot)++;
+                m[(*NbrMot)]=malloc(20*sizeof(char));
+                j=0;
+                }
+            rep=1;
+            }else {
+                rep=0;
+                m[(*NbrMot)][j]=ch[i];
+                j++;
+            }
+        i++;
+    }
+    m[*NbrMot][j]=0;
+    (*NbrMot)++;
+}
+void DateDeRetour(int *jour,int *mois,int *anne,int NbreJour){
+
+        /** AJOUTEZ NbreJour A LA DATE JOUR/MOIS/ANNEE
+         **/
+
+         int i;
+         do{
+            i=MaxJour(*anne,*mois);
+            if ((NbreJour+(*jour))>i) {
+                (*mois)++;
+                NbreJour=NbreJour-i+(*jour)-1;
+                (*jour)=1;
+                if (*mois>12) {
+                    (*anne)++;
+                    (*mois)=1;
+                }
+                }else break;
+         }while(NbreJour>0);
+         (*jour)+=NbreJour;
+}
+void RamplcerCracrter(char *t,char a,char b){
+
+        /// RAMPLACER CRACRTER A PAR B SI IL EXISTE DANS T
+
+    int i=0;
+    while(t[i]!=0){
+        if (t[i]==a) t[i]=b;
+        i++;
+    }
+}
+int DiffiranceEntre2Date(int jour1,int mois1 ,int annee1,int jour2,int mois2,int annee2){
+
+        /** CALCULE LE NOMBRE DE JOUR QUE DATE1 ARRIVE A DATE2
+         ** C'EST LA DATE1 DEJA DEPASSER DATE2 ON RETURN 0
+         **/
+
+         if (annee1>annee2) return 0;
+         if ((annee1==annee2)&&(mois1>mois2)) return 0;
+         if ((annee1==annee2)&&(mois1==mois2)&&(jour1>=jour2)) return 0;
+         int i=0;
+         do{
+            i++;
+            DateDeRetour(&jour1,&mois1,&annee1,1);
+         }while((annee1<annee2)||(mois1<mois2)||(jour1<jour2));
+    return i;
+}
+int strstrcase(char* ch,char* rh){
+
+    /// RECHARCHEZ C'EST RH EXISTE DANS CH ET INGORER LE UPPER CASE
+
+    int i=0,j=0;
+    while(ch[i]!=0 && rh[j]!=0){
+        if (tolower(ch[i])==tolower(rh[j])) j++;
+            else {
+                j=0;
+            if (tolower(ch[i])==tolower(rh[j])) j++;
+            }
+            i++;
+        }
+    if (rh[j]==0) return 1;
+        else return 0;
+    }
+
+#endif
